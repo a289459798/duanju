23737611941a515c00092499ead83ce900cabafb
zhangzy <zhangzy0816@gmail.com>
2023-07-24 18:01:11 +0800

    增加追剧的数据库操作

--- app/action/historyAction.ts
+++ app/action/historyAction.ts
@@ -30,4 +30,36 @@ export default {
       }, ${data.duration}, '${new Date().toLocaleString()}')`,
     );
   },
+
+  getFollow: async () => {
+    const results = await global.db?.executeSql(
+      'select * from Follow order by time desc',
+    );
+    if (results?.[0].rows?.length > 0) {
+      let res = [];
+      for (let i = 0; i < results?.[0].rows?.length; i++) {
+        res.push(results?.[0].rows.item(i));
+      }
+      return res;
+    }
+  },
+  addFollow: async (data: {id: number; index: number; duration: number}) => {
+    const results = await global.db?.executeSql(
+      'select * from Follow where id=?',
+      [data.id],
+    );
+    if (results?.[0].rows?.length > 0) {
+      global.db?.executeSql(
+        `update Follow set duration=${data.duration},current = ${
+          data.index
+        },time='${new Date().toLocaleString()}'  where id=${data.id} `,
+      );
+      return;
+    }
+    global.db?.executeSql(
+      `insert into Follow(id, current, duration, time) values(${data.id}, ${
+        data.index
+      }, ${data.duration}, '${new Date().toLocaleString()}')`,
+    );
+  },
 };
