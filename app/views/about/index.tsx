499c1adb00331dfe76521a86b14847a3051ce6b4
zhangzy <zhangzy0816@gmail.com>
2023-07-24 15:47:27 +0800

    关于我们页面

--- app/views/about/index.tsx
+++ app/views/about/index.tsx
@@ -4,8 +4,9 @@ import DeviceInfo from 'react-native-device-info';
 
 import {Text} from '@/component';
 import config from '@/config';
-import {CreatePage} from '@/utils';
+import {CreatePage, Screen} from '@/utils';
 import screen from '@/utils/screen';
+import {Image} from '@rneui/themed';
 
 export default CreatePage({
   navigationProps: () => ({
@@ -14,14 +15,22 @@ export default CreatePage({
   Component: () => {
     return (
       <View style={styles.container}>
+        <Image style={styles.logo} source={{uri: ''}} />
         <Text
           style={{
             color: '#fff',
             fontSize: screen.calc(16),
             marginTop: screen.calc(20),
-            flex: 1,
           }}>
           {config.AppName}
+        </Text>
+        <Text
+          style={{
+            color: '#666',
+            fontSize: screen.calc(14),
+            marginBottom: screen.calc(20),
+          }}>
+          精彩短剧尽在{config.AppName}
         </Text>
         <Text
           style={{
@@ -31,6 +40,19 @@ export default CreatePage({
           }}>
           v{DeviceInfo.getVersion()}
         </Text>
+
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            paddingBottom: Screen.calc(100),
+          }}>
+          <Text style={styles.proto}>《用户协议》</Text>
+          <Text style={styles.proto}>《隐私政策》</Text>
+          <Text style={styles.proto}>《收集个人信息明示清单》</Text>
+          <Text style={styles.proto}>《个人信息第三方共享清单》</Text>
+        </View>
       </View>
     );
   },
@@ -40,5 +62,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    paddingTop: Screen.calc(30),
+  },
+  logo: {
+    width: Screen.calc(100),
+    height: Screen.calc(100),
+    backgroundColor: 'red',
+  },
+  proto: {
+    color: 'blue',
+    marginTop: Screen.calc(10),
   },
 });
